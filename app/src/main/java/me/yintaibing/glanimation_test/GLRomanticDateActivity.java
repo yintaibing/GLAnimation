3f6eb6a4b4e19c465c4bde8c03875477162e5597
yintaibing <471411745@qq.com>
2019-03-07 10:49:05 +0800

    实现GLRomanticDate

--- app/src/main/java/me/yintaibing/glanimation_test/GLRomanticDateActivity.java
+++ app/src/main/java/me/yintaibing/glanimation_test/GLRomanticDateActivity.java
@@ -17,17 +17,17 @@ import me.yintaibing.glanimation.GLView;
 public class GLRomanticDateActivity extends Activity {
     String DIR = "sdcard/com.zhenai.android/file/gift/3/";
 
-    String FILE_FLOWER                  = DIR + "bg_live_video_romantic_date_flower.png";
-    String FILE_FLOWER_ETC              = DIR + "bg_live_video_romantic_date_flower.pkm";
-    String FILE_FLOWER_ETC_ALPHA        = DIR + "bg_live_video_romantic_date_flower_alpha.pkm";
-
-    String FILE_LEFT_HAND               = DIR + "icon_live_video_romantic_date_left_hand.png";
-    String FILE_LEFT_HAND_ETC           = DIR + "icon_live_video_romantic_date_left_hand.pkm";
-    String FILE_LEFT_HAND_ETC_ALPHA     = DIR + "icon_live_video_romantic_date_left_hand_alpha.pkm";
-
-    String FILE_RIGHT_HAND              = DIR + "icon_live_video_romantic_date_right_hand.png";
-    String FILE_RIGHT_HAND_ETC          = DIR + "icon_live_video_romantic_date_right_hand.pkm";
-    String FILE_RIGHT_HAND_ETC_ALPHA    = DIR + "icon_live_video_romantic_date_right_hand_alpha.pkm";
+    String FILE_HEART_LIGHT     = DIR + "bg_live_video_romantic_date_heart_light.png";
+    String FILE_HEART_BLINK_1   = DIR + "icon_live_video_romantic_date_heart_light_blink_1.png";
+    String FILE_HEART_BLINK_2   = DIR + "icon_live_video_romantic_date_heart_light_blink_2.png";
+    String FILE_FLOWER          = DIR + "bg_live_video_romantic_date_flower.png";
+    String FILE_FLOWER_LIGHT_1  = DIR + "icon_live_video_romantic_date_flower_light_1.png";
+    String FILE_FLOWER_LIGHT_2  = DIR + "icon_live_video_romantic_date_flower_light_2.png";
+    String FILE_FLOWER_LIGHT_3  = DIR + "icon_live_video_romantic_date_flower_light_3.png";
+    String FILE_LEFT_HAND       = DIR + "icon_live_video_romantic_date_left_hand.png";
+    String FILE_RIGHT_HAND      = DIR + "icon_live_video_romantic_date_right_hand.png";
+    String FILE_FLOAT_HEART     = DIR + "icon_live_video_romantic_date_float_heart.png";
+    String FILE_FALLING         = DIR + "bg_live_video_romantic_date_falling.png";
 
     GLSurfaceViewExt glSurfaceViewExt;
     private List<GLView> mGLViews;
@@ -49,11 +49,48 @@ public class GLRomanticDateActivity extends Activity {
     private void romanticDate() {
         mGLViews = new ArrayList<>();
 
+        addHeartLight();
+        addHeartBlinks();
         addFlower();
+        addFlowerLights();
         addLeftHand();
         addRightHand();
+        addFloatHeart();
+        addFalling();
 
         glSurfaceViewExt.render(mGLViews);
+    }
+
+    private void addHeartLight() {
+        GLView heartLight = new GLView("heartLight");
+
+        GLView.GLLayoutParams lp = new GLView.GLLayoutParams();
+        lp.widthRatio = 1.0f;
+        lp.heightRatio = 0.5f;
+        lp.z = 0.1f;
+        lp.gravity = Gravity.CENTER;
+        heartLight.setLayoutParams(lp);
+
+        heartLight.setTexture(etc1(FILE_HEART_LIGHT));
+
+        mGLViews.add(heartLight);
+    }
+
+    private void addHeartBlinks() {
+        for (int i = 0; i < 2; i++) {
+            GLView blink = new GLView(i == 0 ? "heartBlink1" : "heartBlink2");
+
+            GLView.GLLayoutParams lp = new GLView.GLLayoutParams();
+            lp.widthRatio = 1.0f;
+            lp.heightRatio = 0.5f;
+            lp.z = 0.2f;
+            lp.gravity = Gravity.CENTER;
+            blink.setLayoutParams(lp);
+
+            blink.setTexture(etc1(i == 0 ? FILE_HEART_BLINK_1 : FILE_HEART_BLINK_2));
+
+            mGLViews.add(blink);
+        }
     }
 
     private void addFlower() {
@@ -62,12 +99,11 @@ public class GLRomanticDateActivity extends Activity {
         GLView.GLLayoutParams lp = new GLView.GLLayoutParams();
         lp.widthRatio = 1.0f;
         lp.heightRatio = 0.5f;
-        lp.z = 0.2f;
+        lp.z = 0.3f;
         lp.gravity = Gravity.BOTTOM;
         flower.setLayoutParams(lp);
 
-        GLTexture texture = new GLTexture(FILE_FLOWER_ETC, FILE_FLOWER_ETC_ALPHA);
-        flower.setTexture(texture);
+        flower.setTexture(etc1(FILE_FLOWER));
 
         GLAnimation animation = new GLTranslateAnimation(
                 GLAnimation.RELATIVE_TO_SELF, 0f, GLAnimation.RELATIVE_TO_SELF, 0f,
@@ -80,6 +116,24 @@ public class GLRomanticDateActivity extends Activity {
         mGLViews.add(flower);
     }
 
+    private void addFlowerLights() {
+        String[] files = {FILE_FLOWER_LIGHT_1, FILE_FLOWER_LIGHT_2, FILE_FLOWER_LIGHT_3};
+        for (int i = 0; i < 3; i++) {
+            GLView light = new GLView("flowerLight" + i);
+
+            GLView.GLLayoutParams lp = new GLView.GLLayoutParams();
+            lp.widthRatio = 1.0f;
+            lp.heightRatio = 0.5f;
+            lp.z = 0.4f;
+            lp.gravity = Gravity.CENTER;
+            light.setLayoutParams(lp);
+
+            light.setTexture(etc1(files[i]));
+
+            mGLViews.add(light);
+        }
+    }
+
     private void addLeftHand() {
         GLView leftHand = new GLView("leftHand");
 
@@ -90,17 +144,11 @@ public class GLRomanticDateActivity extends Activity {
         lp.gravity = Gravity.CENTER_VERTICAL;
         leftHand.setLayoutParams(lp);
 
-        GLTexture texture = new GLTexture(FILE_LEFT_HAND_ETC, FILE_LEFT_HAND_ETC_ALPHA);
-        leftHand.setTexture(texture);
+        leftHand.setTexture(etc1(FILE_LEFT_HAND));
 
         GLAnimation animation = new GLTranslateAnimation(
                 GLAnimation.RELATIVE_TO_SELF, -1f, GLAnimation.RELATIVE_TO_SELF, 0f,
                 GLAnimation.RELATIVE_TO_SELF, 0f, GLAnimation.RELATIVE_TO_SELF, 0f);
-////        GLAnimation animation = new GLRotateAnimation(0f, 360f,
-////                GLAnimation.RELATIVE_TO_PARENT, 0.75f,
-////                GLAnimation.RELATIVE_TO_SELF, 0.5f);
-//        GLAnimation animation = new GLScaleAnimation(1f, 0f, 1f, 0f,
-//                GLAnimation.RELATIVE_TO_PARENT, 1f, GLAnimation.RELATIVE_TO_SELF, 0.5f);
         animation.setDuration(2000L);
         animation.setFillBefore(true);
         animation.setFillAfter(true);
@@ -115,12 +163,11 @@ public class GLRomanticDateActivity extends Activity {
         GLView.GLLayoutParams lp = new GLView.GLLayoutParams();
         lp.widthRatio = 0.5f;
         lp.height = 600;
-        lp.z = 0.8f;
+        lp.z = 0.5f;
         lp.gravity = Gravity.END | Gravity.CENTER_VERTICAL;
         rightHand.setLayoutParams(lp);
 
-        GLTexture texture = new GLTexture(FILE_RIGHT_HAND_ETC, FILE_RIGHT_HAND_ETC_ALPHA);
-        rightHand.setTexture(texture);
+        rightHand.setTexture(etc1(FILE_RIGHT_HAND));
 
         GLAnimation animation = new GLTranslateAnimation(
                 GLAnimation.RELATIVE_TO_SELF, 1f, GLAnimation.RELATIVE_TO_SELF, 0f,
@@ -132,5 +179,57 @@ public class GLRomanticDateActivity extends Activity {
         rightHand.setAnimation(animation);
 
         mGLViews.add(rightHand);
+    }
+
+    private void addFloatHeart() {
+        GLView floatHeart = new GLView("floatHeart");
+
+        GLView.GLLayoutParams lp = new GLView.GLLayoutParams();
+        lp.widthRatio = 0.5f;
+        lp.height = 600;
+        lp.z = 0.6f;
+        lp.gravity = Gravity.CENTER;
+        floatHeart.setLayoutParams(lp);
+
+        floatHeart.setTexture(etc1(FILE_FLOAT_HEART));
+
+        GLAnimation animation = new GLTranslateAnimation(
+                GLAnimation.RELATIVE_TO_SELF, 0f, GLAnimation.RELATIVE_TO_SELF, 0f,
+                GLAnimation.RELATIVE_TO_SELF, 0f, GLAnimation.RELATIVE_TO_PARENT, 0f);
+        animation.setDuration(2000L);
+        animation.setFillBefore(true);
+        animation.setFillAfter(true);
+        floatHeart.setAnimation(animation);
+
+        mGLViews.add(floatHeart);
+    }
+
+    private void addFalling() {
+        GLView falling = new GLView("falling");
+
+        GLView.GLLayoutParams lp = new GLView.GLLayoutParams();
+        lp.widthRatio = 1.0f;
+        lp.heightRatio = 1.0f;
+        lp.z = 0.7f;
+        falling.setLayoutParams(lp);
+
+        falling.setTexture(etc1(FILE_FALLING));
+
+        GLAnimation animation = new GLTranslateAnimation(
+                GLAnimation.RELATIVE_TO_SELF, 0f, GLAnimation.RELATIVE_TO_SELF, 0f,
+                GLAnimation.RELATIVE_TO_PARENT, -1f, GLAnimation.RELATIVE_TO_PARENT, 0f);
+        animation.setDuration(5000L);
+        animation.setFillBefore(true);
+        animation.setFillAfter(true);
+        falling.setAnimation(animation);
+
+        mGLViews.add(falling);
+    }
+
+    private GLTexture etc1(String pngFile) {
+        String name = pngFile.substring(0, pngFile.length() - 4);
+        String etc1 = name + ".pkm";
+        String etc1Alpha = name + "_alpha.pkm";
+        return new GLTexture(etc1, etc1Alpha);
     }
 }
